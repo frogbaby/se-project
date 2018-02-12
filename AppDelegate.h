e3a3281cb311869d5e59293e95027a6bcc0f41c8
wadeyandanping <2289623250@qq.com>
2018-02-12 09:39:00 -0600

    #1

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstApp
+//
+//  Created by danping yan on 1/17/18.
+//  Copyright © 2018 danping yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
